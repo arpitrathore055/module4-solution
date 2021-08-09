ddcef40f20fc77363b23455a4cdc3f4134f17519
Arpit <arpitrathore009@gmail.com>
2021-08-09 11:07:16 +0530

    updated array

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var names=new Array();
-names[0]="Arpit";
-names[1]="Jonathan";
-names[2]="Adarsh";
-names[3]="Johnny";
-names[4]="Shreyansh";
-names[5]="John";
-names[6]="Jeff";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
 
 for (let i = 0; i < names.length; i++) {
     if(names[i].charAt(0)=='j'||names[i].charAt(0)=='J')
